7534ffce0b0572e1001d5e693a5fc0687fb61565
Timo Mueller <mueller@mogwailabs.de>
2019-05-02 13:58:43 +0200

    Updated build scripts

--- buildAndCopyMeterpreter.sh
+++ buildAndCopyMeterpreter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -r /home/timo/Tools/bachelor/metasploit-framework/data/meterpreter/*; 
-scp -r mogwai@192.168.42.128:C:/Users/Mogwai/Documents/GitHub/metasploit-payloads/c/meterpreter/output/x64/ /home/timo/Tools/bachelor/metasploit-framework/data/meterpreter/;
-mv /home/timo/Tools/bachelor/metasploit-framework/data/meterpreter/x64/* /home/timo/Tools/bachelor/metasploit-framework/data/meterpreter/; 
+rm -r /home/timo/Projects/bachelor/metasploit_framework/data/meterpreter/*; 
+scp -r mogwai@192.168.42.128:C:/Users/Mogwai/Documents/GitHub/metasploit-payloads/c/meterpreter/output/x64/ /home/timo/Projects/bachelor/metasploit_framework/data/meterpreter/;
+mv /home/timo/Projects/bachelor/metasploit_framework/data/meterpreter/x64/* /home/timo/Projects/bachelor/metasploit_framework/data/meterpreter/; 
 ./msfconsole -r ~/Projects/bachelor/msf.rc 
