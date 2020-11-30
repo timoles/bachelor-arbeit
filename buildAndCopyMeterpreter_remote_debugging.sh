57ba32572b80cf152a95085185dcfd04a38e0ef7
Timo Müller <timoles@users.noreply.github.com>
2020-11-30 20:37:37 +0100

    Delete buildAndCopyMeterpreter_remote_debugging.sh

--- buildAndCopyMeterpreter_remote_debugging.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -r /home/timo/Projects/bachelor/metasploit_framework/data/meterpreter/*; 
-scp -r mogwai@192.168.42.128:C:/Users/Mogwai/Documents/GitHub/metasploit-payloads/c/meterpreter/output/x64/ /home/timo/Projects/bachelor/metasploit_framework/data/meterpreter/;
-mv /home/timo/Projects/bachelor/metasploit_framework/data/meterpreter/x64/* /home/timo/Projects/bachelor/metasploit_framework/data/meterpreter/; 
-/opt/ruby/lib/ruby/gems/2.6.0/gems/ruby-debug-ide-0.6.1/bin/rdebug-ide  --host 127.0.0.1 --port 1234 -- ~/Projects/bachelor/metasploit_framework/msfconsole
